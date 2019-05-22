e0067638f7fa10d964d9be9267d01899380566f9
MIGUELAN <miguelangelrtorresco@gmail.com>
2019-05-22 11:11:34 -0500

    Adicion del codigo para enviar y recibir datos

--- ARDUINO_ESCLAVO3/ARDUINO_ESCLAVO3.ino
+++ ARDUINO_ESCLAVO3/ARDUINO_ESCLAVO3.ino
@@ -1,16 +1,27 @@
 #include <Wire.h>
 #define address 32
+int VALOR;
+int LED1,LED2;
 
 void setup() {
   Wire.setClock(400000);
   Wire.begin(address);
-  Wire.onReceive(receiver); 
+  //Wire.onReceive(receiver); 
+  Wire.onRequest(sender);
+  pinMode(0,INPUT);
+  pinMode(1,INPUT); 
 }
 
 void loop() {
+
 }
 
-void receiver(int number){
-  while(Wire.available() > 0){
-    }
+void sender(){
+    VALOR= analogRead (A0);
+    Wire.write(VALOR);
+    LED1 = digitalRead (0);
+    Wire.write(LED1);
+    LED2 = digitalRead (1);
+    Wire.write(LED2);
+    
 }
